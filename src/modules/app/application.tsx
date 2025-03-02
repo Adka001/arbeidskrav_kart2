40e8397d699bcd9a680a7653629042e6235cbbad
adriankastrati <adka006@student.kristiania.no>
2025-03-02 14:42:44 +0100

    1

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -62,7 +62,7 @@ const osmLayer = new TileLayer({
 
 const defenceLayer = new VectorLayer({
   source: new VectorSource({
-    url: "arbeidskrav_kart2/public/geojson/Sivilforsvarsdistrikter.geojson",
+    url: "https://adka001.github.io/arbeidskrav_kart2/geojson/Sivilforsvarsdistrikter.geojson",
     format: new GeoJSON(),
   }),
   style: polygonStyle,
@@ -70,7 +70,7 @@ const defenceLayer = new VectorLayer({
 
 const shelterLayer = new VectorLayer({
   source: new VectorSource({
-    url: "arbeidskrav_kart2/public/geojson/Offentligetilfluktsrom.geojson",
+    url: "https://adka001.github.io/arbeidskrav_kart2/geojson/offentligetilfluktsrom.geojson",
     format: new GeoJSON(),
   }),
   style: pointStyle,
